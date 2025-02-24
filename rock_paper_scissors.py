1182a76a1bac921cbb5b683b3dc6262f7d3429de
Milena Davudova <mdavudova22@gmail.com>
2025-02-24 23:27:49 +0000

    EDIT: combined homework

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,8 +5,9 @@ def game():
     number_of_rounds = 3
     current_round = 0
     print("You are playing rock, paper, scissors")
+
     while current_round < number_of_rounds:
-        players_choice = input("Please type R for rock, P for paper and S for scissors").strip().upper()
+        players_choice = input("Please type R for rock, P for paper and S for scissors: ").strip().upper()
         players_input_dict = {"R": "rock", "P": "paper", "S": "scissors"}
         computers_input_dict = {0: "rock", 1: "paper", 2: "scissors"}
         player = players_input_dict[players_choice]
@@ -18,21 +19,26 @@ def game():
         if player == computer:
             print("It's a tie, choose again.")
             continue
-        elif player == "rock":
-            if computer == "scissors":
-                print("You won!")
-            else:
-                print("You lost!")
-        elif player == "scissors":
-            if computer == "paper":
-                print("You won!")
-            else:
-                print("You lost!")
-        elif player == "paper":
-            if computer == "rock":
-                print("You won!")
-            else:
-                print("You lost!")
+        elif (player == 'rock' and computer == 'scissors') or (player == 'paper' and computer == 'rock') or (
+                player == 'scissors' and computer == 'paper'):
+            print("You win!")
+        else:
+            print("You lose!")
+        # elif player == "rock":
+        #     if computer == "scissors":
+        #         print("You won!")
+        #     else:
+        #         print("You lost!")
+        # elif player == "scissors":
+        #     if computer == "paper":
+        #         print("You won!")
+        #     else:
+        #         print("You lost!")
+        # elif player == "paper":
+        #     if computer == "rock":
+        #         print("You won!")
+        #     else:
+        #         print("You lost!")
         current_round += 1
 
 
